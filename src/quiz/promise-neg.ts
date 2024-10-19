7fdd5c8d3061bc1f696a4387a59360c027e2410d
supra <rikc.2000@gmail.com>
2024-10-18 23:04:12 -0400

    bug fix

--- src/quiz/promise-neg.ts
+++ src/quiz/promise-neg.ts
@@ -1,39 +1,39 @@
 const array2D_3 = [
-  [1, 2, 3, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 - 1],
+  [
+    1, 2, 3, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 4, 5, 3, 4, 5, 6, 3, 3, 3, 2, 1,
+    2, 4, 5, 6, 7, 8, 9, 3, 1, 6, 5, 4, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3,
+    2, 1, 1, 2, 1, 2, 1, 2, 5, 4, 3, 2, 1, 0, 1, 2, 3, 2, 3, 4, 5, 6, 7, 8, 9,
+    8, 7, 6, 4, 5, 3, 4, 5, 6, 3, 3, 3, 2, 1, 2, 4, 5, 6, 7, 8, 9, 3, 1, 6, 5,
+    4, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 1, 2, 1, 2, 5, 4, 3,
+    2, 1, 0, 1, 2, 3, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 4, 5, 3, 4, 5, 6, 3, 3,
+    3, 2, 1, 2, 4, 5, 6, 7, 8, 9, 3, 1, 6, 5, 4, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6,
+    5, 4, 3, 2, 1, 1, 2, 1, 2, 1, 2, 5, 4, 3, 2, 1, 0, -1,
+  ],
   [4, 5, 6],
-  [7, 8, -9],
+  [-9],
 ];
 
-function sumNeg2DArrayConcurrent(arr: number[][]): Promise<number> {
+function findNegativeInRow(arr: number[][]): Promise<string> {
   return new Promise((resolve, reject) => {
     if (arr.length === 0) {
       reject("Cannot sum an empty array");
     }
 
-    const sumPromises = arr.map((row, rowIdx) => {
-      return new Promise<number>((resolve) => {
-        let foundNegative = false;
-        let rowSum = 0;
-
+    const promises = arr.map((row, rowIdx) => {
+      return new Promise<string>((resolve) => {
         for (let i = 0; i < row.length; i++) {
           if (row[i] < 0) {
-            foundNegative = true;
+            resolve("Found negative number in row: " + rowIdx);
           }
-          rowSum += row[i];
         }
 
-        if (foundNegative) {
-          console.log(`Found negative number in row: ${rowIdx + 1}`);
-        }
-
-        resolve(rowSum);
+        resolve("No negative number found in row: " + rowIdx);
       });
     });
 
-    Promise.all(sumPromises)
-      .then((rowSums) => {
-        const totalSum = rowSums.reduce((acc, val) => acc + val, 0);
-        resolve(totalSum);
+    Promise.any(promises)
+      .then((verdict) => {
+        resolve(verdict);
       })
       .catch((error) => {
         reject("Failed to compute the sum: " + error);
@@ -41,10 +41,10 @@ function sumNeg2DArrayConcurrent(arr: number[][]): Promise<number> {
   });
 }
 
-sumNeg2DArrayConcurrent(array2D_3)
-  .then((sum) => {
-    console.log(`Sum of 2D array: ${sum}`);
+findNegativeInRow(array2D_3)
+  .then((verdict) => {
+    console.log(verdict);
   })
   .catch((error) => {
-    console.error(`Failed to sum 2D array: ${error}`);
+    console.error(`Error: ${error}`);
   });
